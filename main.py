0e777a978635046467a5a5ea70f9a8c433d7f8c7
berayco <55498165+berayco@users.noreply.github.com>
2023-12-12 23:54:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ def get_hex_color_codes(image):
     pil_image = Image.fromarray(cv2.cvtColor(resized_image, cv2.COLOR_BGR2RGB))
 
     colors = pil_image.getcolors(40*40)
+    total_pixels = sum(count for color, count in colors)
     color_counts = Counter({'#{:02x}{:02x}{:02x}'.format(*color[1]): color[0] for color in colors})
     most_common_colors = color_counts.most_common(20)
-    return [color[0] for color in most_common_colors]
+    return [(color[0], color[1] / total_pixels * 100) for color in most_common_colors]
 
-def display_color(hex_color):
+def display_color(hex_color, percentage):
     st.markdown(
-        f"<div style='display: inline-block; margin: 10px; padding: 10px; background-color: {hex_color}; color: white; border-radius: 5px;'>{hex_color}</div>", 
+        f"<div style='display: inline-block; margin: 10px; padding: 10px; background-color: {hex_color}; color: white; border-radius: 5px;'>{hex_color} - {percentage:.2f}%</div>", 
         unsafe_allow_html=True)
 
 def main():
@@ -27,9 +28,9 @@ def main():
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
         hex_codes = get_hex_color_codes(image)
-        st.write("En Sık Bulunan 20 Hex Renk Kodu:")
-        for hex_color in hex_codes:
-            display_color(hex_color)
+        st.write("En Sık Bulunan 20 Hex Renk Kodu ve Yüzdelikleri:")
+        for hex_color, percentage in hex_codes:
+            display_color(hex_color, percentage)
         st.image(image, caption='Yüklenen Resim', use_column_width=True)
 
 if __name__ == "__main__":
