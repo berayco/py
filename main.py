5583d9760611c072bd105ae95fd37257eeffab56
berayco <55498165+berayco@users.noreply.github.com>
2023-12-12 23:44:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,53 +2,36 @@ import streamlit as st
 import cv2
 from PIL import Image
 import numpy as np
-
+from collections import Counter
 
 def get_hex_color_codes(image):
-    # Resmi OpenCV formatına dönüştür
-    image = np.array(image)
+    image = np.array(image.convert('RGB'))  # Arka planı olmayan resimler için
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-    # Renk sayısını azaltmak için resmi küçült
     resized_image = cv2.resize(image, (40, 40), interpolation=cv2.INTER_AREA)
-
-    # PIL Image'e dönüştür
     pil_image = Image.fromarray(cv2.cvtColor(resized_image, cv2.COLOR_BGR2RGB))
 
-    # Baskın renkleri çıkar
     colors = pil_image.getcolors(40*40)
-    hex_colors = ['#{:02x}{:02x}{:02x}'.format(*color[1]) for color in colors]
+    color_counts = Counter({'#{:02x}{:02x}{:02x}'.format(*color[1]): color[0] for color in colors})
+    most_common_colors = color_counts.most_common(20)
+    return [color[0] for color in most_common_colors]
 
-    return hex_colors
-
-# Renk kutusunu gösterme fonksiyonu
 def display_color(hex_color):
-    # Renk kutusunu ve hex kodunu yan yana göster
     st.markdown(
-        f"<div style='display: inline-block; margin: 10px; padding: 10px; background-color: {hex_color}; color: white; border-radius: 5px;'>{hex_color}</div>",
+        f"<div style='display: inline-block; margin: 10px; padding: 10px; background-color: {hex_color}; color: white; border-radius: 5px;'>{hex_color}</div>", 
         unsafe_allow_html=True)
 
-# Streamlit uygulamasının ana yapısı
 def main():
     st.title("Resimden Hex Renk Kodu Çıkarıcı")
-
-    # Dosya yükleme widget'ı
-    uploaded_file = st.file_uploader("Bir PNG dosyası yükle", type=["png"])
+    uploaded_file = st.file_uploader("Bir resim dosyası yükle (PNG, JPEG, JPG)", type=["png", "jpeg", "jpg"])
 
     if uploaded_file is not None:
-        # PIL ile resmi yükle
         image = Image.open(uploaded_file)
-
-        # Hex renk kodlarını elde et
         hex_codes = get_hex_color_codes(image)
-
-        # Renk kodlarını ve renkleri göster
-        st.write("Bulunan Hex Renk Kodları ve Renkler:")
+        st.write("En Sık Bulunan 20 Hex Renk Kodu:")
         for hex_color in hex_codes:
             display_color(hex_color)
-
-        # Renkleri görselleştir (opsiyonel)
         st.image(image, caption='Yüklenen Resim', use_column_width=True)
 
 if __name__ == "__main__":
     main()
+
